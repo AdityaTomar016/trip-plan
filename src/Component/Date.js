1056dd3e3adad6f675cd42e4bad11d2fab6a5f92
Bhumika-arora-09 <84390051+Bhumika-arora-09@users.noreply.github.com>
2023-11-26 22:28:12 +0530

    first commit

--- src/Component/Date.js
+++ src/Component/Date.js
@@ -1,11 +1,11 @@
 import 'date-fns';
 import React from 'react';
-// import Grid from '@material-ui/core/Grid';
-// import DateFnsUtils from '@date-io/date-fns';
-// import {
-//   MuiPickersUtilsProvider,
-//   KeyboardDatePicker,
-// } from '@material-ui/pickers';
+import Grid from '@mui/material/Grid';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
 
 export default function DatePicker(props) {
   
--- src/Component/Preferences.js
+++ src/Component/Preferences.js
@@ -1,8 +1,8 @@
 import React from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
-// import Typography from '@material-ui/core/Typography';
-// import Slider from '@material-ui/core/Slider';
-// import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+import Slider from '@mui/material/Slider';
+import TextField from '@mui/material/TextField';
 
 const useStyles = makeStyles({
   root: {
